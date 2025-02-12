5f9d3cc773d92f13ab5b0daf62b46805dcc16e84
boursyt <boursyt@gmail.com>
2025-02-12 14:56:07 +0100

    FEAT: récuperation des info des different fichier (s_gestionFile) dans le directory utilisateur. (name, size, type et date. ). Creation de la vue et du template associé

--- filemanager/services/s_gestionFile.py
+++ filemanager/services/s_gestionFile.py
@@ -1,35 +1,59 @@
+import math
 import os
-class file:
+from datetime import datetime
+class File:
 
-    def __init__(self, name, size, date, type, owner):
-        self.name = name
-        self.path = "../filedir"
-        self.size = size
-        self.date = date
-        self.type = type
-        self.owner = owner
-
+    def convert_size(self, size_bytes):
+        """
+        Convert a size in bytes to a human-readable format
+        """
+        if size_bytes == 0:
+            return "0B"
+        size_name = ("B", "KB", "MB", "GB", "TB")
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return f"{s} {size_name[i]}"
 
     def getFile(self, path, user):
         """
-        Get every file in the path
+        Get every file in the path and return some informations about it
+
         """
         try:
-            files = os.listdir(f'{path}/{user}')
-            return files
+            files = os.listdir(f'/home/theo/PycharmProjects/M1_dockerOnline/{path}/{user}')
+            filesList = {
+                'name': [],
+                'size': [],
+                'date': [],
+                'type': [],
+            }
+            for file in files:
+                filesList['name'].append(file)
+                size=os.path.getsize(f'{path}/{user}/{file}')
+                filesList['size'].append(self.convert_size(size))
+                filesList['date'].append(datetime.fromtimestamp(os.path.getctime(f'{path}/{user}/{file}')))
+                filesList['type'].append(os.path.splitext(file)[1])
+            print(filesList)
+            return filesList
         except Exception as e:
+            print("no file")
+            print(e)
             return {'error': str(e)}
 
 
-    def importFile(self, path, user, file):
+    def importFile(self, path, user,name,file):
         """
         Import file in the path
         """
         try:
             with open(f'{path}/{user}/{file.name}', 'wb+') as destination:
-                for chunk in file.chunks():
-                    destination.write(chunk)
-            return {'success': 'File imported'}
+                if name in os.listdir(f'{path}/{user}'):
+                    return {'error': 'File already exists'}
+                else:
+                    for chunk in file.chunks():
+                        destination.write(chunk)
+                    return {'success': 'File imported'}
         except Exception as e:
             return {'error': str(e)}
 
--- filemanager/views/v_file.py
+++ filemanager/views/v_file.py
@@ -1,8 +1,42 @@
-from filemanager.services.s_gestionFile import file
+from filemanager.services.s_gestionFile import File
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 
-def file(request):
+def Showfile(request):
+    """
+    Show file in the user directory and send it to the template
+    """
+    user = request.user
+    fileListe=listeFile(request)
+    context = {
+        'menu': {
+            'page': 'home'
+        },
+        'files':fileListe
+    }
+    return render(request, 'file.html', context)
 
-    return render(request, 'file.html')
+def listeFile(request):
+    """
+    Show file in the user directory and send it to the template
+    """
+    user = request.user
+    fileList = File().getFile('filedir', user)
+
+    # Vérifiez si fileList contient une erreur
+    if 'error' in fileList:
+        print(fileList['error'])
+        return []
+
+    files = []
+    for i in range(len(fileList['name'])):
+        files.append({
+            "name": fileList["name"][i],
+            "size": fileList["size"][i],
+            "date": fileList["date"][i],
+            "type": fileList["type"][i]
+        })
+
+    print(files)
+    return files
